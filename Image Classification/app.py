7467c9081903761eb4c6b7c305b0aef2f08bc2c1
sahilmurhekar <sahil.murhekar2004@gmail.com>
2023-10-05 12:25:45 +0530

    First commit

---
+++ Image Classification/app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+from clarifai_utils.modules.css import ClarifaiStreamlitCSS
+
+st.set_page_config(layout="wide")
+
+ClarifaiStreamlitCSS.insert_default_css(st)
+
+from langchain.llms import Clarifai
+
+# Title & Sidebar Image
+
+st.title("🦙Chat with Llama 2 (70B) Model")
+st.sidebar.image("https://clarifai.com/favicon.svg", width=100)
+
+# Get the Clarifai API Key
+
+with st.sidebar:
+    clarifai_pat = st.text_input("69e53a6270c34e5ba3586adb57a4907e", type="password")
+
+
+# Function to Generate Response
+
+def generate(text):
+    llm = Clarifai(pat=clarifai_pat, user_id='meta', app_id='Llama-2', model_id='llama2-70b-chat')
+    st.info(llm(text))
+
+# Form that takes text as imput and return the response from the Model
+with st.form("my_form"):
+    text = st.text_area("Enter text:", "How to Evaluate ML Models?")
+    submitted = st.form_submit_button("Submit")
+    if not clarifai_pat:
+        st.info("Please add your Clarifai PAT to continue.")
+    elif submitted:
+        generate(text)
